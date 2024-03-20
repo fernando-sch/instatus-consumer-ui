9aed4c62101db36516d48b99edc968ffe8c88ad9
Fernando Schneider <93660206+fernando-sch@users.noreply.github.com>
2024-03-20 19:30:27 -0300

    Add use fetch incidents hook (#4)
    * add .env exemple
    
    * add project config folder
    
    * add axios client
    
    * add incident and incident update dto
    
    * add use fetch incidents hook

---
+++ src/incidents/hooks/use-fetch-incidents.ts
@@ -0,0 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { axiosClient } from '@/services/axios-client';
+
+import { Incident, buildIncidentDTO } from '@/incidents/dto/incident';
+
+const useFetchIncidentsQuery = () => {
+  return useQuery<Incident[]>({
+    queryKey: ['incidents'],
+    queryFn: () => axiosClient.get('/incidents').then((response) => response.data.data.map(buildIncidentDTO)),
+  });
+};
+
+export const useFetchIncidents = () => {
+  const { data: incidents = [] } = useFetchIncidentsQuery();
+  return { incidents };
+};
